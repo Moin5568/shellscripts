3d69c2e432c7333101c7b6699bfff244f9a0c38f
Moin Shaikh <moin241397@gmail.comm>
2024-08-20 15:08:01 +0530

    this is my 2 commit

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
-echo "we are learning git commands"
+echo "we are learning Git commands"
 
 
