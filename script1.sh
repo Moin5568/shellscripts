f0c1a0b9097cd1edb0625780d23554bb241ddc0c
Moin Shaikh <moin241397@gmail.comm>
2024-08-21 10:29:19 +0530

    IC241097 is ready

--- script1.sh
+++ script1.sh
@@ -2,5 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "we are learning Git commands"
-
+echo "We are learning git branching strtegy"
 
