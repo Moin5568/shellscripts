2e0288cd8280f810c80f43dc6745a2ab66ed1aae
Moin Shaikh <moin241397@gmail.comm>
2024-08-22 11:08:13 +0530

    hello

--- script1.sh
+++ script1.sh
@@ -3,4 +3,5 @@ echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Git/Github"
 echo "we are learning Git commands"
 echo "We are learning git branching strtegy"
+echo "We are learning git for devops"
 
